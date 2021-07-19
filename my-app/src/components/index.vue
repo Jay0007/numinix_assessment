c2fd8a6336e8b64efa55657901e7385b524c1bce
Priyank Patel <pbpatel23796@gmail.com>
2021-07-19 18:22:36 -0400

    v.1
    iteration 1

---
+++ my-app/src/components/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols=1>
+        <searchFilter/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="card in cards" :key="card.id" cols="3">
+        <card :customerInfo="card"></card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import card from './customers/card';
+  import searchFilter from './customers/searchFilter';
+
+  export default {
+    components: {
+      card,
+      searchFilter
+    },
+
+    created () {
+      this.$axios.get('http://jsonplaceholder.typicode.com/users')
+        .then(response => this.cards = response.data)
+    },
+
+    data: () => ({
+      cards: []
+    }),
+  };
+</script>
