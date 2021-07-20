28834221448ce326a62f97e5a395e223fbc0185f
Priyank Patel <pbpatel23796@gmail.com>
2021-07-20 16:24:58 -0400

    unnecessary code removed

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import { store } from './store/store'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
@@ -11,6 +10,5 @@ Vue.prototype.$axios = axios
 new Vue({
   vuetify,
   VueAxios,
-  store,
   render: h => h(App)
 }).$mount('#app')
